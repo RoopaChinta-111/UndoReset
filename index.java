ba6e0cd4c7ddabf84aa4d211225783372a76247f
Roopa <roopa.chinta111@gmail.com>
2024-10-22 17:39:15 +0530

    file added

---
+++ index.java
@@ -0,0 +1,8 @@
+class A{
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("GITHUB");
+      Sytem.out.println("GITHUB undo");
+      Sytem.out.println("undo");
+	}
+}
